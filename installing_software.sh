0d73aa8945f7d9d24a2709fdb4ae7bd92c90e148
Mary Medoye <129941983+Cmieytee@users.noreply.github.com>
2023-08-07 20:21:55 +0200

    Installing software

---
+++ installing_software.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Script to install git"
+
+echo "Installation started"
+
+if [ "$(uname)" == "Linux" ];
+
+then
+
+echo "this is linux box, installing git"
+
+yum install git -y
+
+elif [ "$(uname)" == "Darwin" ]
+
+then
+
+echo "this is not linux box"
+
+echo "this is Macos"
+
+brew install git
+
+else 
+
+echo "not installing"
+
+fi
